0acc410cc4669639db517dad45d8a0465e75995f
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-02-21 22:39:00 -0400

    Create ExpressionComun.java

---
+++ app/src/main/java/practica1patronesdiseno/interpreterEjercicio6/ExpressionComun.java
@@ -0,0 +1,15 @@
+package practica1patronesdiseno.interpreterEjercicio6;
+
+public class ExpressionComun extends TerminoFormato{
+    @Override
+    public void interpreter(Context context) {
+        
+            context.output=context.output+" "+context.input.split(" ",2)[0];
+            String [] aux = context.input.split(" ", 2);
+            if(aux.length>1){
+                context.input=aux[1];
+            }
+            
+        
+    }
+}
