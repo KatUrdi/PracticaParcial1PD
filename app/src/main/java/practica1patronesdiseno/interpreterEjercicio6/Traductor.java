62cfd50eeef550b3e297755a4c7ba9b0a4a92d69
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-02-21 22:41:17 -0400

    Create Traductor.java

---
+++ app/src/main/java/practica1patronesdiseno/interpreterEjercicio6/Traductor.java
@@ -0,0 +1,44 @@
+package practica1patronesdiseno.interpreterEjercicio6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Traductor extends TerminoFormato{
+    private List<TerminoFormato> historiaUsuario = new ArrayList<>();
+    private Context context;
+
+    public Traductor(String historiaUsuarioOG){
+        context = new Context(historiaUsuarioOG);
+        for (String msg: historiaUsuarioOG.split(" ")){
+            switch(msg){
+                case "Como":
+                    historiaUsuario.add(new ExpressionComo());
+                    break;
+                case "Quiero":
+                    historiaUsuario.add(new ExpressionQuiero());
+                    break;
+                case "Para":
+                    historiaUsuario.add(new ExpressionPara());
+                    break;
+                default:
+                    historiaUsuario.add(new ExpressionComun());
+                    break;
+                
+                }
+        }
+    }
+
+    public String traduccion(){
+        for(TerminoFormato terminoFormato:historiaUsuario){
+            terminoFormato.interpreter(context);
+        }
+        return context.output;
+    }
+
+    @Override
+    public void interpreter(Context context) {
+        
+    }
+
+
+}
