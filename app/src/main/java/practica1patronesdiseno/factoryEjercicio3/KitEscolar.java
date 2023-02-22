42c78ce114d84f6e7a582b63f1e763a606c41c8d
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-02-21 21:44:14 -0400

    Create KitEscolar.java

---
+++ app/src/main/java/practica1patronesdiseno/factoryEjercicio3/KitEscolar.java
@@ -0,0 +1,45 @@
+package practica1patronesdiseno.factoryEjercicio3;
+
+public class KitEscolar implements IKit{
+
+    private Mochila mochila;
+    private Cuaderno cuaderno;
+    private Deportivo deportivo;
+
+    public Cuaderno getCuaderno() {
+        return cuaderno;
+    }
+
+    public KitEscolar setCuaderno(Cuaderno cuaderno) {
+        this.cuaderno = cuaderno;
+        return this;
+    }
+
+    public Deportivo getDeportivo() {
+        return deportivo;
+    }
+
+    public KitEscolar setDeportivo(Deportivo deportivo) {
+        this.deportivo = deportivo;
+        return this;
+    }
+
+    @Override
+    public Mochila getMochila() {
+        return mochila;
+    }
+
+    @Override
+    public void setMochila(Mochila mochila) {
+        this.mochila=mochila;
+    }
+
+    @Override
+    public void show() {
+        System.out.println("Kit Escolar");
+        mochila.show();
+        cuaderno.show();
+        deportivo.show();
+    }
+    
+}
