4e22f915c7dcc276f811e8f1016f0ea538434ffa
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-02-21 22:57:06 -0400

    Create FactoryCajero.java

---
+++ app/src/main/java/practica1patronesdiseno/singletonMultithreadAbstractFactoryEjercicio8/FactoryCajero.java
@@ -0,0 +1,31 @@
+package practica1patronesdiseno.singletonMultithreadAbstractFactoryEjercicio8;
+
+public class FactoryCajero {
+    public static ICajero createCajero(String nombreCajero, String ciCajero, String tipo){
+
+        ICajero cajero;
+
+        switch(tipo){
+            case "vivienda":
+                cajero = new CajeroVivienda();
+                break;
+            case "negocios":
+            cajero = new CajeroNegocios();
+                break;
+            case "financiamiento":
+            cajero = new CajeroFinanciamiento();
+                break;
+            case "creditos":
+            cajero = new CajeroCreditos();
+                break;
+            default:
+            cajero = new CajeroVivienda();
+        }
+
+        cajero.setCiCajero(ciCajero);
+        cajero.setNombreCajero(nombreCajero);
+
+        return cajero;
+
+    }
+}
