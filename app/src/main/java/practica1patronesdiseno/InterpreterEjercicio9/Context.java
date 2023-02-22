3b3ea36fbfa96ca6c99bc145bc216b137c00c86b
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-02-21 23:00:02 -0400

    Create Context.java

---
+++ app/src/main/java/practica1patronesdiseno/InterpreterEjercicio9/Context.java
@@ -0,0 +1,10 @@
+package practica1patronesdiseno.InterpreterEjercicio9;
+
+public class Context {
+    protected String input="";
+    protected String output="";
+
+    public Context(String input){
+        this.input=input;
+    }
+}
