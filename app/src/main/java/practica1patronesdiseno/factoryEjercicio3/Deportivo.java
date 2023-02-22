0d189a23b83df8acc284838825952a0436968b0b
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-02-21 21:39:08 -0400

    Create Deportivo.java

---
+++ app/src/main/java/practica1patronesdiseno/factoryEjercicio3/Deportivo.java
@@ -0,0 +1,39 @@
+package practica1patronesdiseno.factoryEjercicio3;
+
+public class Deportivo {
+    private String talla;
+    private String color;
+    private int numeroPrendas;
+
+    public Deportivo(String talla, String color, int numeroPrendas){
+        this.talla=talla;
+        this.color=color;
+        this.numeroPrendas=numeroPrendas;
+    }
+
+    public String getTalla() {
+        return talla;
+    }
+    public void setTalla(String talla) {
+        this.talla = talla;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public int getNumeroPrendas() {
+        return numeroPrendas;
+    }
+    public void setNumeroPrendas(int numeroPrendas) {
+        this.numeroPrendas = numeroPrendas;
+    }
+
+    public void show(){
+        System.out.println("Deportivo: talla "+talla+", color "+color+", "+numeroPrendas+" prendas");
+    }
+
+    
+
+}
