6a9bd9172d053a97c7cb92b2790f84458d50315c
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-02-21 23:04:18 -0400

    Create Cabina.java

---
+++ app/src/main/java/practica1patronesdiseno/singletonIteratorEjercicio10/Cabina.java
@@ -0,0 +1,52 @@
+package practica1patronesdiseno.InterpreterEjercicio9;
+
+import java.util.Stack;
+
+public class Cabina implements IAggregate {
+    private static Cabina instance = null;
+    private final double peaje=5.50;
+    private double caja=0;
+    private Stack<Vehiculo> registrados;
+
+    private Cabina(){
+        registrados = new Stack<>();
+    }
+
+    public static Cabina getInstance(){
+        if(instance == null){
+            instance = new Cabina();
+        }
+        return instance;
+    }
+
+    public void pagarPeaje(Vehiculo vehiculo){
+        if(registrados.contains(vehiculo)){
+            System.out.println("El vehiculo ya realizo el pago de peaje");
+        } else{
+            caja=caja+peaje;
+            registrados.add(vehiculo);
+            if(registrados.size()==6){
+                iterate();
+            }
+        }
+    }
+
+    public void iterate(){
+
+        System.out.println("Cabina");
+        System.out.println("Monto recaudado: "+caja+" BS");
+        System.out.println("lista vehiculos");
+        Iterator iterator = createIterator();
+        while(iterator.hasNext()){
+            Vehiculo aux = iterator.next();
+            aux.show();
+        }
+    }
+
+    @Override
+    public Iterator createIterator() {
+       return new CabinaIterator(registrados);
+    }
+
+    
+}
