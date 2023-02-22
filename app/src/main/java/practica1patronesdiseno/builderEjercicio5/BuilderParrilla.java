72604d404758c998b34b1bfd7b3197325d4b4700
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-02-21 22:07:20 -0400

    Create BuilderParrilla.java

---
+++ app/src/main/java/practica1patronesdiseno/builderEjercicio5/BuilderParrilla.java
@@ -0,0 +1,17 @@
+package practica1patronesdiseno.iteratorEjercicio4;
+
+public abstract class BuilderParrilla {
+    protected Parrilla parrilla;
+
+    public Parrilla getParrilla(){
+        return parrilla;
+    }
+
+    public void prepararParrilla(){
+        parrilla = new Parrilla();
+    }
+
+    public abstract void buildTipoCarne();
+    public abstract void buildSaborRefresco();
+    public abstract void buildGuarniciones();
+}
